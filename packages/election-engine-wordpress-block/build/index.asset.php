13b7383d7c1b49887c2a88817cd5b3dd4fae5e3f
Jason Norwood-Young <jason@10layer.com>
2024-05-15 15:48:01 +0200

    Major update

--- packages/election-engine-wordpress-block/build/index.asset.php
+++ packages/election-engine-wordpress-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'cd4abd647ac6cc50fd3c');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-i18n'), 'version' => '4021cd86f6afde8f0537');
